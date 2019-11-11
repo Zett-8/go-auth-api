86fad0385cc0d1d106fd3fbaf7597e337632a3d0
toshiki.kamei <toshiki.kamei>
2019-11-11 12:23:22 +0900

    added handler

--- router.go
+++ router.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"go-auth-api/handler"
 	"net/http"
 )
 
@@ -13,13 +14,11 @@ func newRouter() *echo.Echo {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Go auth API")
 	})
 
 	api := e.Group("/api")
-	api.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	api.GET("/test", handler.GetTodos)
 
 	return e
 }
