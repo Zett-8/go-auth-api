ac2439dce7d35355b655b0a7ccc4cdbb4de3ab3e
toshiki.kamei <toshiki.kamei>
2019-11-11 14:25:02 +0900

    feature login

--- router.go
+++ router.go
@@ -13,10 +13,14 @@ func newRouter() *echo.Echo {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	//index
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Go auth API")
 	})
+
+	//auth
 	e.POST("/sign-up", handler.SignUp)
+	e.POST("/login", handler.Login)
 
 	//api := e.Group("/api")
 	//api.GET("/user", handler.GetUser)
